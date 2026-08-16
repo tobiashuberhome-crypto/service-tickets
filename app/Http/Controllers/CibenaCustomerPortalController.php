db69fd1b8ef9a5476b83be59b9b9e174b1e0ad2b
tobiashuberhome-crypto <tobias.huber.home@gmail.com>
2026-08-16 21:48:12 +0200

    Fix Cibena portal account scope and customer id

--- app/Http/Controllers/CibenaCustomerPortalController.php
+++ app/Http/Controllers/CibenaCustomerPortalController.php
@@ -6,6 +6,7 @@ use App\Models\CustomerPortalAccount;
 
 class CibenaCustomerPortalController extends GeiserCustomerPortalController
 {
+    protected const CUSTOMER_ID = 10;
     protected const SESSION_KEY = 'cibena_customer_portal_account_id';
     protected const PORTAL_SCOPE = CustomerPortalAccount::PORTAL_SCOPE_CIBENA;
     protected const PORTAL_ROUTE_PREFIX = 'cibena-portal';
